454cc3e1905aaf77c1372c14c2abe88464dcf373
brachafrid <brachi6721236@gmail.com>
2021-10-11 19:28:26 +0300

    customer

---
+++ dotNet5782_1974_0044/DAL/Costumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+
+        }
+    }
+}
