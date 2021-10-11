0a32b078087597f0e535a8edd00cf2eb7ab1c617
brachafrid <brachi6721236@gmail.com>
2021-10-11 16:25:08 +0300

    inital code+welcome function

---
+++ dotNet5782_1974_0044/Targil0/Program1974.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome1974();
+            welcome0044();
+            Console.ReadKey();
+        }
+        private static void welcome1974()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void welcome0044();
+    }
+}
