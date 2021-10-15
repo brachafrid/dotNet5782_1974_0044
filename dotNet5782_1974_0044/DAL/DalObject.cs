304c8bf1f6959fae2c31eebaea58a6a339e67bf0
YehuditShw <ys113048@gmail.com>
2021-10-15 10:50:12 +0300

    dalobgect

--- dotNet5782_1974_0044/DAL/DalObject.cs
+++ dotNet5782_1974_0044/DAL/DalObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.DO;
 
 namespace DalObject
 {
@@ -12,6 +13,35 @@ namespace DalObject
         {
             DataSorce.Initialize();
         }
-        
+        public void addStation(int id, string name, double longitude, double latitude)
+        {
+            if (DataSorce.Config.idxStations <= DataSorce.STATIONS_LENGTH - 1)
+                throw new IndexOutOfRangeException("The array is full");
+            if (latitude > 180 || latitude < 0)
+                throw new ArgumentOutOfRangeException("");
+            if(longitude < 0 || longitude > 90)
+                throw new ArgumentOutOfRangeException("");
+            DataSorce.stations[DataSorce.STATIONS_LENGTH].Id = id;
+            DataSorce.stations[DataSorce.STATIONS_LENGTH].Name = name;
+            DataSorce.stations[DataSorce.STATIONS_LENGTH].Latitude = latitude;
+            DataSorce.stations[DataSorce.STATIONS_LENGTH].Longitude = longitude;
+
+
+        }
+        public void addCustomer(int id, string phone, string name, double Longitude, double Lattitude)
+        {
+
+        }
+        public void addDrone(int id, string model,WeightCategories MaxWeight, DroneStatuses Status, double Battery)
+        {
+
+        }
+        public void packageReception(int Id, int SenderId, int TargetId, WeightCategories Weigth, Prioripies Priority)
+        {
+
+        }
+
+
+
     }
 }
--- dotNet5782_1974_0044/DAL/DataSorce.cs
+++ dotNet5782_1974_0044/DAL/DataSorce.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IDAL.DO;
- 
+
 
 namespace DalObject
 {
-    class DataSorce
+    public class DataSorce
     {
-        const int DRONE_LENGTH = 10;
-        const int STATIONS_LENGTH = 5;
-        const int CUSTOMERS_LENGTH = 100;
-        const int PARCELS_LENGTH = 1000;
+        public const int DRONE_LENGTH = 10;
+        public const int STATIONS_LENGTH = 5;
+        public const int CUSTOMERS_LENGTH = 100;
+        public const int PARCELS_LENGTH = 1000;
         internal static Drone[] drones = new Drone[DRONE_LENGTH];
         internal static Station[] stations = new Station[STATIONS_LENGTH];
         internal static Customer[] customers = new Customer[CUSTOMERS_LENGTH];
@@ -22,16 +22,16 @@ namespace DalObject
         internal class Config
         {
             internal static int idxDrones = 0;
-            internal static int idxStations=0;
-            internal static int idxCustomers=0;
-            internal static int idxParcels=0;
+            internal static int idxStations = 0;
+            internal static int idxCustomers = 0;
+            internal static int idxParcels = 0;
 
-            static int IdParcel =0;
+            static int IdParcel = 0;
         }
 
         static internal void Initialize()
         {
-               
+
         }
     }
 
