2a5ca22d31e031beb6ba4d22c69ae7c756a2d729
YehuditShw <ys113048@gmail.com>
2021-10-19 21:10:42 +0300

    ii

--- dotNet5782_1974_0044/ConsoleUI/Program.cs
+++ dotNet5782_1974_0044/ConsoleUI/Program.cs
@@ -5,23 +5,27 @@ namespace ConsoleUI
 
     class Program
     {
-        enum Menu { Add, Update, Display,DisplayList, Exit }
-        enum Add { Drone, Station, Parcel, Customer}
+        enum Menu { Add, Update, Display, DisplayList, Exit }
+        enum Add { Drone, Station, Parcel, Customer }
         enum Update { }
-        enum DisplayList {Stations, Drones,Customer,Parcels, AvailableChargingStations, }
+        enum DisplayList { Stations, Drones, Customer, Parcels, AvailableChargingStations, }
         enum Display { }
         static void Main(string[] args)
         {
-
+           
         }
-        static public void DisplayMenu(Enum en)
+        static public void DisplayMenu(Type en)
         {
             int idx = 0;
             foreach (var item in Enum.GetValues(typeof(en)))
             {
                 Console.WriteLine(item + " press " + idx++);
             }
+            
         }
+
+
+
 
     }
 }
