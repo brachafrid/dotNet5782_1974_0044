0af3b311e4d7d9e87a9150a13cd59922d620a7ae
brachafrid <brachi6721236@gmail.com>
2021-10-21 14:43:08 +0300

    switch

--- dotNet5782_1974_0044/ConsoleUI/Program.cs
+++ dotNet5782_1974_0044/ConsoleUI/Program.cs
@@ -4,33 +4,80 @@ namespace ConsoleUI
 {
     class Program
     {
-        enum Menu { Addi, Update, Display, DisplayList, Exit }
+        enum Menu { Add, Update, Display, DisplayList, Exit }
         enum Add { Drone, Station, Parcel, Customer }
-        enum Update {AssingParcelToDrone,CollectParcelByDrone,SupplyParcelToDestination,SendingDroneForCharging,RealsingDroneFromCharghing }
+        enum Update { AssingParcelToDrone, CollectParcelByDrone, SupplyParcelToDestination, SendingDroneForCharging, RealsingDroneFromCharghing }
         enum DisplayList { Stations, Drones, Customer, Parcels, AvailableChargingStations, }
-        enum Display { Station,Drone,Customer,Parcel}
-      
+        enum Display { Station, Drone, Customer, Parcel }
+
         static void Main(string[] args)
         {
-            DisplayMenu(typeof(Menu));
+            DisplayMenus(typeof(Menu));
             int choice;
-            int.TryParse(Console.ReadLine(),out choice);
-            switch (choice)
+            Menu option;
+            do
             {
-                case :
-                    DisplayMenu(typeof(Add));
-                default:
-            }
-            
-        }
-        static public void DisplayMenu(Type en)
-        {
-                int idx = 0;
-                foreach (var item in Enum.GetValues(en)) 
+                int.TryParse(Console.ReadLine(), out choice);
+                option = (Menu)choice;
+                switch (option)
                 {
-                    Console.WriteLine(item + " press " + idx++);
-                }  
-            
+                    case Menu.Add:
+                        DisplayMenus(typeof(Add));
+                        break;
+                    case Menu.Update:
+                        DisplayMenus(typeof(Update));
+                        break;
+                    case Menu.Display:
+                        DisplayMenus(typeof(Display));
+                        break;
+                    case Menu.DisplayList:
+                        DisplayMenus(typeof(DisplayList));
+                        break;
+                    case Menu.Exit:
+                        break;
+                    default:
+                        break;
+
+                }
+            } while (option != Menu.Exit);
+
+
+        }
+        static public void DisplayMenus(Type en)
+        {
+            int idx = 0;
+            foreach (var item in Enum.GetValues(en))
+            {
+                Console.WriteLine(item + " press " + idx++);
+            }
+
+        }
+
+        static public void switchAdd()
+        {
+            int choice;
+            Add option;
+            int.TryParse(Console.ReadLine(), out choice);
+            option = (Add)choice;
+            switch (option)
+            {
+                case Add.Drone:
+                    DalObject.DalObject.
+                    break;
+                case Add.Station:
+                    DisplayMenus(typeof(Update));
+                    break;
+                case Add.Parcel:
+                    DisplayMenus(typeof(Display));
+                    break;
+                case Add.Customer:
+                    DisplayMenus(typeof(DisplayList));
+                    break;
+                default:
+                    break;
+
+            }
+
         }
 
 
