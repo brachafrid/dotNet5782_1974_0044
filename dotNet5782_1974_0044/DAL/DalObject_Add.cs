dedd01a7932165f5b4e9fc30d9d01ef7eb329c31
YehuditShw <ys113048@gmail.com>
2021-10-21 16:59:45 +0300

    fix

--- dotNet5782_1974_0044/DAL/DalObject_Add.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Add.cs
@@ -29,7 +29,12 @@ namespace DalObject
             newCustomer.Longitude = longitude;
             DataSorce.customers.Add(newCustomer);
         }
-        public  void addDrone(string model, WeightCategories MaxWeight)
+        /// <summary>
+        ///  gets parameters and create new drone 
+        /// </summary>
+        /// <param name="model"> drone's model</param>
+        /// <param name="MaxWeight"> max weight that the drone can swipe (light- 0,medium - 1,heavy - 2)</param>
+        public void addDrone(string model, WeightCategories MaxWeight)
         {
             Drone newDrone = new Drone();
             newDrone.Id = ++DataSorce.Config.idxDrones;
@@ -39,7 +44,14 @@ namespace DalObject
             newDrone.Battery = 100;
             DataSorce.drones.Add(newDrone);
         }
-        public  void parcelsReception(int SenderId, int TargetId, WeightCategories Weigth, Prioripies Priority)
+        /// <summary>
+        /// gets parameters and create new parcel 
+        /// </summary>
+        /// <param name="SenderId"> id of sener</param>
+        /// <param name="TargetId"> id of target</param>
+        /// <param name="Weigth"> weigth of parcel (light- 0,medium - 1,heavy - 2)</param>
+        /// <param name="Priority"> priority of send the parcel (regular - 0,fast - 1,emergency - 2)</param>
+        public void parcelsReception(int SenderId, int TargetId, WeightCategories Weigth, Prioripies Priority)
         {
             Parcel newParcel = new Parcel();
             newParcel.Id = ++DataSorce.Config.idxParcels;
