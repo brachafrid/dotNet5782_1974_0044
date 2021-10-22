da1f6338f9289ff6acab440712adedc77f33c57b
brachafrid <brachi6721236@gmail.com>
2021-10-22 11:28:28 +0300

    try

--- dotNet5782_1974_0044/DAL/DalObject_Add.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Add.cs
@@ -12,10 +12,10 @@ namespace DalObject
         /// <summary>
         /// help array to convert for sexgesimal base
         /// </summary>
-       private const char[] helpSexagesimal = new char[59]{'0','1','2','3','4','5','6','7','8','9',
+       private char[] helpSexagesimal = new char[]{'0','1','2','3','4','5','6','7','8','9',
                      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                      'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
-        const char[] = q { 'l' };
+
         /// <summary>
         ///  Gets parameters and create new station 
         /// </summary>
