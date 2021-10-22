757e60ca84b257ba6f89da2c0f7d8b614c9f7695
brachafrid <brachi6721236@gmail.com>
2021-10-22 11:31:16 +0300

    try

--- dotNet5782_1974_0044/DAL/DalObject_Add.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Add.cs
@@ -14,7 +14,7 @@ namespace DalObject
         /// </summary>
        private char[] helpSexagesimal = new char[]{'0','1','2','3','4','5','6','7','8','9',
                      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'});
+                     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
         /// <summary>
         ///  Gets parameters and create new station 
         /// </summary>
