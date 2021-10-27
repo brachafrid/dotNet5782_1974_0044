5651f43d799867d260395867c11e47b4cef3823b
YehuditShw <ys113048@gmail.com>
2021-10-27 15:37:51 +0300

    check id

--- dotNet5782_1974_0044/DAL/DalObject_Add.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Add.cs
@@ -11,12 +11,6 @@ namespace DalObject
     public partial class DalObject
     {
         /// <summary>
-        /// help array to convert for sexgesimal base
-        /// </summary>
-        private char[] helpSexagesimal = new char[]{'0','1','2','3','4','5','6','7','8','9',
-                    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x'};
-        /// <summary>
         ///  Gets parameters and create new station 
         /// </summary>
         /// <param name="name"> Station`s name</param>
@@ -32,8 +26,6 @@ namespace DalObject
             newStation.Latitude = latitude;
             newStation.Longitude = longitude;
             newStation.ChargeSlots = chargeSlots;
-            newStation.latitudeSexagesimal = DoubleToString(newStation.Latitude, helpSexagesimal);
-            newStation.longitudeSexagesimal = DoubleToString(newStation.Longitude, helpSexagesimal);
             DataSorce.stations.Add(newStation);
         }
         /// <summary>
@@ -43,7 +35,7 @@ namespace DalObject
         /// <param name="name">The customer`s name</param>
         /// <param name="longitude">>The position of the customer in relation to the longitude</param>
         /// <param name="latitude">>The position of the customer in relation to the latitude</param>
-        public void addCustomer(int id,string phone, string name, double longitude, double latitude)
+        public void addCustomer(int id, string phone, string name, double longitude, double latitude)
         {
             uniqueIDTaxCheck<Customer>(DataSorce.customers, id);
             Customer newCustomer = new Customer();
@@ -52,8 +44,6 @@ namespace DalObject
             newCustomer.Phone = phone;
             newCustomer.Latitude = latitude;
             newCustomer.Longitude = longitude;
-            newCustomer.latitudeSexagesimal = DoubleToString(newCustomer.Latitude, helpSexagesimal);
-            newCustomer.longitudeSexagesimal = DoubleToString(newCustomer.Longitude, helpSexagesimal);
             DataSorce.customers.Add(newCustomer);
         }
         /// <summary>
@@ -94,49 +84,15 @@ namespace DalObject
             newParcel.DorneId = 0;
             DataSorce.parcels.Add(newParcel);
         }
-        /// <summary>
-        /// convert a double number to any base in accordance the array of signs parameter
-        /// </summary>
-        /// <param name="value">The value thar needed to convert</param><param>
-        /// <param name="baseChars">The arrray of signs in the needed base</param>
-        /// <returns>A string that it is the value in the other base</returns>
-        public static string DoubleToString(double value, char[] baseChars)
-        {
-            string result1 = string.Empty;
-            string result2 = string.Empty;
-            int targetBase = baseChars.Length;
-            int valueInt = (int)value;
-            double valueDouble = value - (int)value;
-
-            do
-            {
-                result1 = baseChars[valueInt % targetBase] + result1;
-                valueInt = valueInt / targetBase;
-            }
-            while (valueInt > 0);
-            do
-            {
-                result2 += baseChars[(int)valueDouble % targetBase];
-                valueDouble = (valueDouble % 1) * targetBase;
-            } while (valueDouble > 0);
-            return result1 + '.' + result2;
-        }
-        /// <summary>
-        /// Checks if id is uniqe
-        /// </summary>
-        /// <typeparam name="T">object that has property - "id"</typeparam>
-        /// <param name="lst"> list</param>
-        /// <param name="id"> int </param>
-        private void uniqueIDTaxCheck<T>(List<T> lst, int id)
+        void uniqueIDTaxCheck<T>(List<T> lst, int id)
         {
             foreach (var item in lst)
             {
-                Type t = item.GetType();
-                if ((int)t.GetProperty("id").GetValue(item) == id)
+                if ((int)item.GetType().GetProperty("id").GetValue(item, null) == id)
                     throw new ArgumentException(" An element with the same key already exists in the list");
             }
-
         }
     }
+
 
 }
