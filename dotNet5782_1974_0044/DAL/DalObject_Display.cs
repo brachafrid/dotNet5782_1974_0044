be4861127a0f514f1041cf32265337f43d0a7fb2
brachafrid <brachi6721236@gmail.com>
2021-10-29 11:31:06 +0300

    continue

--- dotNet5782_1974_0044/DAL/DalObject_Display.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Display.cs
@@ -9,20 +9,7 @@ namespace DalObject
 {
     public partial class DalObject
     {
-        /// <summary>
-        /// Find a satation that has tha same id number as the parameter
-        /// </summary>
-        /// <param name="id">The id number of the requested station/param>
-        /// <returns>A station for display</returns>
-        public Station GetStation(int id)
-        {
-            return DataSorce.Stations.First(item => item.Id == id);
-        }
-        /// <summary>
-        /// Find a drone that has tha same id number as the parameter
-        /// </summary>
-        /// <param name="id">The id number of the requested drone</param>
-        /// <returns>A drone for display</returns>
+       
         public Drone GetDrone(int id)
         {
             return DataSorce.Drones.First(item => item.Id == id);
--- dotNet5782_1974_0044/DAL/DalObject_Station.cs
+++ dotNet5782_1974_0044/DAL/DalObject_Station.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.DO;
 
 namespace  DalObjec
 
@@ -27,5 +28,20 @@ namespace  DalObjec
             newStation.ChargeSlots = chargeSlots;
             DataSorce.Stations.Add(newStation);
         }
+
+        /// <summary>
+        /// Find a satation that has tha same id number as the parameter
+        /// </summary>
+        /// <param name="id">The id number of the requested station/param>
+        /// <returns>A station for display</returns>
+        public Station GetStation(int id)
+        {
+            return DataSorce.Stations.First(item => item.Id == id);
+        }
+        /// <summary>
+        /// Find a drone that has tha same id number as the parameter
+        /// </summary>
+        /// <param name="id">The id number of the requested drone</param>
+        /// <returns>A drone for display</returns>
     }
 }
