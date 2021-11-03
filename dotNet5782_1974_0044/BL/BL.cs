8450a5bc7804a3378389255abb059ef52066f4f4
YehuditShw <91978643+YehuditShw@users.noreply.github.com>
2021-11-03 18:21:54 +0200

    interface

---
+++ dotNet5782_1974_0044/BL/BL.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBL.BO;
+using IDAL.DO;
+
+namespace BL
+{
+    class BL : IBL.IBL
+    {
+        void IBL.IBL.AddCustomer(int id, string name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.AddDrone(int id, IBL.BO.WeightCategories MaximumWeight, int stationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.AddStation(int id, string name, Location location, int chargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.DeliveryParcelByDrone(int droneId)
+        {
+            throw new NotImplementedException();
+        }
+
+        IDAL.DO.Customer IBL.IBL.GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<IDAL.DO.Customer> IBL.IBL.GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        IDAL.DO.Drone IBL.IBL.GetDrone(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<IDAL.DO.Drone> IBL.IBL.GetDrones()
+        {
+            throw new NotImplementedException();
+        }
+
+        Parcel IBL.IBL.GetParcel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Parcel> IBL.IBL.GetParcels()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Parcel> IBL.IBL.GetParcelsNotAssignedToDrone()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<IDAL.DO.Station> IBL.IBL.GetSationsWithEmptyChargeSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        IDAL.DO.Station IBL.IBL.GetStation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<IDAL.DO.Station> IBL.IBL.GetStations()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.ParcelCollectionByDrone(int DroneId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.ReceiptParcelForDelivery(int senderCustomerId, int recieveCustomerId, IBL.BO.WeightCategories Weight, IBL.BO.Priorities priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.ReleaseDroneFromCharging(int id, float timeOfCharg)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.SendDroneForCharg(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.UpdateCusomer(int id, string name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.UpdateDrone(int id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBL.IBL.UpdateStation(int id, string name, int chargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- dotNet5782_1974_0044/BL/IBL.cs
+++ dotNet5782_1974_0044/BL/IBL.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IBL.BO;
 
 namespace IBL
 {
     interface IBL
     {
+        public void AddStation(int id, string name, Location location, int chargeSlots);
+        public void AddDrone(int id, BO.WeightCategories MaximumWeight, int stationId);
+        public void AddCustomer(int id, string name, string phone);
+        public void ReceiptParcelForDelivery(int senderCustomerId, int recieveCustomerId, BO.WeightCategories Weight, BO.Priorities priority);
+        public void UpdateDrone(int id, string name);
+        public void UpdateStation(int id, string name, int chargeSlots);
+        public void UpdateCusomer(int id, string name, string phone);
+        public void SendDroneForCharg(int id);
+        public void ReleaseDroneFromCharging(int id, float timeOfCharg);
+        public void ParcelCollectionByDrone(int DroneId);
+        public void DeliveryParcelByDrone(int droneId);
+        public IDAL.DO.Station GetStation(int id);
+        public IDAL.DO.Customer GetCustomer(int id);
+        public IDAL.DO.Parcel GetParcel(int id);
+        public IDAL.DO.Drone GetDrone(int id);
+        public IEnumerable<IDAL.DO.Station> GetStations();
+        public IEnumerable<IDAL.DO.Customer> GetCustomers();
+        public IEnumerable<IDAL.DO.Parcel> GetParcels();
+        public IEnumerable<IDAL.DO.Drone> GetDrones();
+        public IEnumerable<IDAL.DO.Parcel> GetParcelsNotAssignedToDrone();
+        public IEnumerable<IDAL.DO.Station> GetSationsWithEmptyChargeSlots();
 
     }
 }
