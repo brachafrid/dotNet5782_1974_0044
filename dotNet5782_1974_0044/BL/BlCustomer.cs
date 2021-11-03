43bc9f403edaf86c1f86531a990438be79b26036
YehuditShw <91978643+YehuditShw@users.noreply.github.com>
2021-11-03 19:51:10 +0200

    public

---
+++ dotNet5782_1974_0044/BL/BlCustomer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+   public partial class BL : IBL.IBL
+    {
+        public void AddCustomer(int id, string name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+        public IDAL.DO.Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<IDAL.DO.Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateCusomer(int id, string name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotNet5782_1974_0044/BL/BlStation.cs
@@ -0,0 +1,34 @@
+﻿using IBL.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public partial class BL:IBL.IBL
+    {
+      public  void UpdateStation(int id, string name, int chargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddStation(int id, string name, Location location, int chargeSlots)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<IDAL.DO.Station> GetSationsWithEmptyChargeSlots()
+        {
+            throw new NotImplementedException();
+        }
+        public IDAL.DO.Station GetStation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IDAL.DO.Station> GetStations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
