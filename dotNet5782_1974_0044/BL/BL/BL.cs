45cf3e6202b8cf80002758c306ce9d27f931c4a7
YehuditShw <91978643+YehuditShw@users.noreply.github.com>
2021-11-16 18:45:01 +0200

    remove device

--- dotNet5782_1974_0044/BL/BL/BL.cs
+++ dotNet5782_1974_0044/BL/BL/BL.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using IBL.BO;
 using IDAL.DO;
-using System.Device;
+
 namespace IBL
 {
     public partial class BL : IBL
     {
-        private List<BO.DroneToList> drones;
+        private List<DroneToList> drones;
         private IDAL.IDal dal = new DalObject.DalObject();
         bool ExistsIDTaxCheck<T>(IEnumerable<T> lst, int id)
         {
