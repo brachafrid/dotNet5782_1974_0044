dd69d0ba57e61508e153390b374e3b4ebb47ab36
brachafrid <91977446+brachafrid@users.noreply.github.com>
2021-12-12 19:03:56 +0200

    usercontrol

--- dotNet5782_1974_0044/DAL/DalObject/DalObjectDrone.cs
+++ dotNet5782_1974_0044/DAL/DalObject/DalObjectDrone.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using IDAL.DO;
 
 namespace DalObject
@@ -46,6 +47,7 @@ namespace DalObject
         /// </summary>
         /// <returns>A list of drones</returns>
         public IEnumerable<Drone> GetDrones() => DataSorce.Drones;
+
 
         //-------------------------------------------------Removing-------------------------------------------------------------
         /// <summary>
