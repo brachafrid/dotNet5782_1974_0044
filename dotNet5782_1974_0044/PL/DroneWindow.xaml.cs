05bb98fb0887a0f1c026c9379d8f3d64b3b87020
YehuditShw <ys113048@gmail.com>
2021-12-14 11:53:08 +0200

    update

--- dotNet5782_1974_0044/PL/DroneWindow.xaml.cs
+++ dotNet5782_1974_0044/PL/DroneWindow.xaml.cs
@@ -80,6 +80,7 @@ namespace PL
                 }, stationId);
 
                 MessageBox.Show("add succses");
+
             }
             catch (ThereIsAnObjectWithTheSameKeyInTheListException)
             {
@@ -100,8 +101,20 @@ namespace PL
                 }
         }
 
-        private void close(object sender, RoutedEventArgs e)
+        private void Close(object sender, RoutedEventArgs e)
         {
+
+        }
+         private void UpdateDataContent(object sender)
+        {
+            object tmp = sender;
+            while(tmp.GetType() != typeof(TabControl))
+                tmp = (tmp as FrameworkElement).Parent;
+            foreach( var item in (tmp as TabControl).Items)
+            {
+
+            }
+            
 
         }
     }
