05bb42982713f1e25d64e2645239a3d39f3c991c
brachafrid <brachi6721236@gmail.com>
2021-12-20 17:17:44 +0200

    factory finishhhhhhhhh

--- dotNet5782_1974_0044/BL/IBL/IBlDrone.cs
+++ dotNet5782_1974_0044/BL/IBL/IBlDrone.cs
@@ -4,18 +4,22 @@ using System;
 
 
 
-namespace IBL
+namespace BL
 {
-    public interface IBlDrone
+    namespace BLApi
     {
-        public void AddDrone(BO.Drone drone, int stationId);
-        public void UpdateDrone(int id, string model);
-        public void SendDroneForCharg(int id);
-        public void ReleaseDroneFromCharging(int id, float timeOfCharg);
-        public BO.Drone GetDrone(int id);
-        public IEnumerable<BO.DroneToList> GetDrones();
-        public void AssingParcelToDrone(int droneId);
-        public void ParcelCollectionByDrone(int DroneId);
-        public void DeliveryParcelByDrone(int droneId);
+        public interface IBlDrone
+        {
+            public void AddDrone(BO.Drone drone, int stationId);
+            public void UpdateDrone(int id, string model);
+            public void SendDroneForCharg(int id);
+            public void ReleaseDroneFromCharging(int id, float timeOfCharg);
+            public BO.Drone GetDrone(int id);
+            public IEnumerable<BO.DroneToList> GetDrones();
+            public void AssingParcelToDrone(int droneId);
+            public void ParcelCollectionByDrone(int DroneId);
+            public void DeliveryParcelByDrone(int droneId);
+        }
     }
+
 }
