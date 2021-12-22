734f38bcfb4751a8162284d444678a4ac0c2eb86
brachafrid <91977446+brachafrid@users.noreply.github.com>
2021-12-22 19:54:32 +0200

    changes

---
+++ dotNet5782_1974_0044/PL/Hundlers/HundlerEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL.PO;
+using BLApi;
+
+namespace PL
+{
+    public static class HundlerEntity
+    {
+        static IBL ibal = BLFactory.GetBL();
+        public static BO.Location ConvertLocation(Location location)
+        {
+            return new BO.Location()
+            {
+               Longitude=location.Longitude,
+               Latitude=location.Latitude
+            };
+        }
+    }
+}
