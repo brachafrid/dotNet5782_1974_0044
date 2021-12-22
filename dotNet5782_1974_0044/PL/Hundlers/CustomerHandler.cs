49534b7bdbc33001b95a42b6113664308807d2f3
TamarKleiman <Ttichnut2@gmail.com>
2021-12-22 20:09:40 +0200

    handler customer

--- dotNet5782_1974_0044/PL/Hundlers/CustomerHandler.cs
+++ dotNet5782_1974_0044/PL/Hundlers/CustomerHandler.cs
@@ -18,25 +18,38 @@ namespace PL.Hundlers
         }
         public Customer GetCustomer(int id)
         {
-            BO.Customer customerBL = ibal.GetCustomer(id);
-            ConvertCustomer(customerBL);
+            //BO.Customer customerBL = ibal.GetCustomer(id);
+            return ConvertBackCustomer(ibal.GetCustomer(id));
         }
         public IEnumerable<CustomerToList> GetCustomers()
         {
-
+           return ibal.GetCustomers();
         }
         public void UpdateCustomer(int id, string name, string phone)
         {
-
+            ibal.UpdateCustomer(id, name, phone);
         }
-        public PO.Customer ConvertCustomer(BO.Customer customer)
+        public BO.Customer ConvertCustomer(PO.Customer customer)
         {
-            return new PO.Customer
+            return new BO.Customer
             {
                 Id = customer.Id,
                 Name = customer.Name,
                 Phone = customer.Phone,
-                Location = new PO.Location { Latitude = customer.Location.Latitude, Longitude = customer.Location.Longitude},
+                Location = new BO.Location { Latitude = customer.Location.Latitude, Longitude = customer.Location.Longitude},
+                FromCustomer = customer.FromCustomer,
+                ToCustomer = customer.ToCustomer
+
+            };
+        }
+        public PO.Customer ConvertBackCustomer(BO.Customer customer)
+        {
+            return new PL.PO.Customer
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Phone = customer.Phone,
+                Location = new PO.Location { Latitude = customer.Location.Latitude, Longitude = customer.Location.Longitude },
                 FromCustomer = customer.FromCustomer,
                 ToCustomer = customer.ToCustomer
 
