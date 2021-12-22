a5e6fbb8feb001d45a0616da7a43d871097a4462
YehuditShw <91978643+YehuditShw@users.noreply.github.com>
2021-12-22 21:17:57 +0200

    parcel

--- dotNet5782_1974_0044/PL/Hundlers/ParcelHandler.cs
+++ dotNet5782_1974_0044/PL/Hundlers/ParcelHandler.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLApi;
+using PL.PO;
 
 namespace PL.Hundlers
 {
-    class ParcelHandler
+    public class ParcelHandler
     {
+        public Parcel ConvertParcel(BO.Parcel parcel)
+        {
+            return new Parcel()
+            {
+                Id = parcel.Id,
+                Weight =(PO.WeightCategories)parcel.Weight ,
+                Piority =(PO.Priorities) parcel.Priority,
+                DeliveryTime = parcel.DeliveryTime,
+                AssignmentTime = parcel.AssignmentTime,
+                CollectionTime = parcel.CollectionTime,
+                CreationTime = parcel.CreationTime,
+                Drone = 
 
+
+            }
+        }
     }
 }
