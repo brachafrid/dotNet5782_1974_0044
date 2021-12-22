a7a6aaa5691d87745d5e6615a841db9faf4f81c2
TamarKleiman <Ttichnut2@gmail.com>
2021-12-22 20:00:09 +0200

    change

---
+++ dotNet5782_1974_0044/PL/Hundlers/CustomerHandler.cs
@@ -0,0 +1,46 @@
+﻿using BLApi;
+using PL.PO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.Hundlers
+{
+    class CustomerHandler
+    {
+        private IBL ibal  = BLFactory.GetBL();
+
+        public void AddCustomer(Customer customer)
+        {
+            ibal.AddCustomer(ConvertCustomer(customer));
+        }
+        public Customer GetCustomer(int id)
+        {
+            BO.Customer customerBL = ibal.GetCustomer(id);
+            ConvertCustomer(customerBL);
+        }
+        public IEnumerable<CustomerToList> GetCustomers()
+        {
+
+        }
+        public void UpdateCustomer(int id, string name, string phone)
+        {
+
+        }
+        public PO.Customer ConvertCustomer(BO.Customer customer)
+        {
+            return new PO.Customer
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Phone = customer.Phone,
+                Location = new PO.Location { Latitude = customer.Location.Latitude, Longitude = customer.Location.Longitude},
+                FromCustomer = customer.FromCustomer,
+                ToCustomer = customer.ToCustomer
+
+            };
+        }
+    }
+}
