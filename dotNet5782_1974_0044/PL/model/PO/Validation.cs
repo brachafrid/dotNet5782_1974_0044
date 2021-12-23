d3599dbdac634ca69663d87799ff447d80f2b18d
YehuditShw <ys113048@gmail.com>
2021-12-23 18:11:34 +0200

    phone

--- dotNet5782_1974_0044/PL/model/PO/Validation.cs
+++ dotNet5782_1974_0044/PL/model/PO/Validation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PL.PO
@@ -13,7 +14,8 @@ namespace PL.PO
         public static bool IdValid(int id) => id > 0;
         public static bool StringValid(string str) => str != string.Empty;
         public static bool LocationValid(Location location) => LongitudeValid(location.Latitude) && LatitudeValid(location.Longitude);
-
+        public static bool PhoneValid(string phone)=>Regex.Match(phone, @"^(\+[0-9]{9})$").Success || Regex.Match(phone, @"^[1-9]\d{10}$").Success|| Regex.Match(phone, @"^[1-9]\d{9}$").Success;
+        
 
     }
 }
