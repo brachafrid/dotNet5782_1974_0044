24e9e0c71d777d77edffd55c210757db082521bc
brachafrid <brachi6721236@gmail.com>
2021-12-28 18:47:45 +0200

    add foldares

---
+++ dotNet5782_1974_0044/PL/model/PO/TabItemFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PL.PO
+{
+    public class TabItemFormat
+    {
+        public string Text { get; set; }
+        public string KindIcon { get; set; }
+        public string TabContent { get; set; }
+    }
+}
