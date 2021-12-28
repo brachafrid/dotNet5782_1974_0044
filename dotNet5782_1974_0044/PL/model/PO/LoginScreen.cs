5b160e6f452f2f91129021849f0d311d9df0ac29
brachafrid <brachi6721236@gmail.com>
2021-12-28 18:23:01 +0200

    changes

---
+++ dotNet5782_1974_0044/PL/model/PO/LoginScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace PL.PO
+{
+    public static class LoginScreen
+    {
+        private static string myScreen { get; set; }
+        public static string MyScreen
+        {
+            get { return myScreen; }
+            set
+            {
+                myScreen = value;
+                NotifyStaticPropertyChanged("MyScreen");
+            }
+        }
+
+        static LoginScreen()
+        {
+            MyScreen = "LoginWindow";
+        }
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+        private static void NotifyStaticPropertyChanged(string propertyName)
+        {
+            if (StaticPropertyChanged != null)
+                StaticPropertyChanged(null, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
