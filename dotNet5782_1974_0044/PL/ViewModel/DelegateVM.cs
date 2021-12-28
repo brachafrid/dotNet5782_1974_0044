db2e8d2045770c8d89769b3ddfad85666dbbbed9
YehuditShw <ys113048@gmail.com>
2021-12-28 18:07:07 +0200

    again

--- dotNet5782_1974_0044/PL/ViewModel/DelegateVM.cs
+++ dotNet5782_1974_0044/PL/ViewModel/DelegateVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PL
 {
-    public delegate object Events(object param = null);
+
     public static class DelegateVM
     {
         public static Action Drone { set; get; }
