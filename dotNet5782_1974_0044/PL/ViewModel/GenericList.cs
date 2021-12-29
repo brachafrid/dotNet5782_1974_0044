0f56f75ff530fe1f59aa79b519de6547f9d3f466
brachafrid <brachi6721236@gmail.com>
2021-12-29 17:03:36 +0200

    merge

--- dotNet5782_1974_0044/PL/ViewModel/GenericList.cs
+++ dotNet5782_1974_0044/PL/ViewModel/GenericList.cs
@@ -6,34 +6,36 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Collections.ObjectModel;
+
 
 namespace PL
 {
     public class GenericList<T> : SortInputVM
     {
-        //public ListCollectionView list { set; get; }
-        //public ListCollectionView SortOption { set; get; }
-        //public string selectedSort { set; get; }
-        //public RelayCommand SortCommand { set; get; }
-       public SortInputVM m;
+        public ListCollectionView list { set; get; }
+        public ObservableCollection<string> SortOption { set; get; }
+        public string selectedSort { set; get; }
+        public RelayCommand SortCommand { set; get; }
+        public SortInputVM m;
 
         public GenericList()
         {
             //list.Filter +=;
             //list.IsLiveFiltering = true;
-            //SortCommand = new(option, null);
-            //UpdateSortOptions();
+            SortCommand = new(option, null);
+            UpdateSortOptions();
             m = new();
             m.te ="lll";
         }
-        //void UpdateSortOptions()
-        //{
-        //    SortOption =new ListCollectionView( typeof(T).GetProperties().Where(prop => prop.PropertyType.IsEnum).Select(prop => prop.Name).ToList());
-        //}
-        //void option(object param)
-        //{
-        //    OptionVisibility = Visibility.Visible;
-        //}
+        void UpdateSortOptions()
+        {
+            SortOption = new ObservableCollection<string>(typeof(T).GetProperties().Where(prop => prop.PropertyType.IsEnum).Select(prop => prop.Name).ToList());
+        }
+            void option(object param)
+        {
+            OptionVisibility = Visibility.Visible;
+        }
 
 
 
--- dotNet5782_1974_0044/PL/ViewModel/SortInputVM.cs
+++ dotNet5782_1974_0044/PL/ViewModel/SortInputVM.cs
@@ -9,25 +9,14 @@ namespace PL
 {
     public class SortInputVM:DependencyObject
     {
-        //public Visibility EnumOptionsVisbility
-        //{
-        //    get { return (Visibility)GetValue(IntOptionsVisbilityProperty); }
-        //    set { SetValue(IntOptionsVisbilityProperty, value); }
-        //}
+        public Visibility EnumOptionsVisbility
+        {
+            get { return (Visibility)GetValue(IntOptionsVisbilityProperty); }
+            set { SetValue(IntOptionsVisbilityProperty, value); }
+        }
 
-        //// Using a DependencyProperty as the backing store for EnumOptionsVisbility.  This enables animation, styling, binding, etc...
-        //public static readonly DependencyProperty IntOptionsVisbilityProperty =
-        //    DependencyProperty.Register("EnumOptionsVisbility", typeof(Visibility), typeof(SortInputVM), new PropertyMetadata(Visibility.Collapsed));
-        //public Visibility OptionVisibility
-        //{
-        //    get { return (Visibility)GetValue(OptionVisibilityProperty); }
-        //    set { SetValue(OptionVisibilityProperty, value); }
-        //}
-
-        //// Using a DependencyProperty as the backing store for OptionVisibility.  This enables animation, styling, binding, etc...
-        //public static readonly DependencyProperty OptionVisibilityProperty =
-        //    DependencyProperty.Register("OptionVisibility", typeof(Visibility), typeof(SortInputVM), new PropertyMetadata(Visibility.Collapsed));
-
-       public string te;
+        // Using a DependencyProperty as the backing store for EnumOptionsVisbility.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IntOptionsVisbilityProperty =
+            DependencyProperty.Register("EnumOptionsVisbility", typeof(Visibility), typeof(SortInputVM), new PropertyMetadata(Visibility.Collapsed));
     }
 }
