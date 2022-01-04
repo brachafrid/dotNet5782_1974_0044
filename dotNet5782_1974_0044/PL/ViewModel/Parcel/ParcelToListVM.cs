1b186e84fe0ef4d903afe3d1d3e6838a9bb0124f
TamarKleiman <Ttichnut2@gmail.com>
2022-01-04 22:17:55 +0200

    changes

--- dotNet5782_1974_0044/PL/ViewModel/Parcel/ParcelToListVM.cs
+++ dotNet5782_1974_0044/PL/ViewModel/Parcel/ParcelToListVM.cs
@@ -13,11 +13,12 @@ namespace PL
     public class ParcelToListVM : GenericList<ParcelToList>
     {
         object id;
-        string state;
+        string state = "";
         public ParcelToListVM()
         {
             UpdateInitList();
             DelegateVM.Parcel += UpdateInitList;
+            DelegateVM.Customer += UpdateInitList;
             DoubleClick = new(OpenDetails, null);
         }
         public ParcelToListVM(object Id, object State)
@@ -27,27 +28,23 @@ namespace PL
             UpdateInitList();
             DelegateVM.Customer += UpdateInitList;
             DelegateVM.Parcel += UpdateInitList;
-
-            //    //object id = Id;
-            //    // Customer customer = PLService.GetCustomer((int)Id);
-            //    //UpdateInitListCustomer();
-            //    //DelegateVM.Parcel += UpdateInitListCustomer;
             DoubleClick = new(OpenDetails, null);
         }
         void UpdateInitList()
         {
-            if(state == "To")
-            {
-                list = new ListCollectionView(PLService.GetCustomer((int)id).ToCustomer.ToList());
-            }
-            if(state == "From")
-            {
-                list = new ListCollectionView(PLService.GetCustomer((int)id).FromCustomer.ToList());
-            }
-            else
+            if (state == "")
             {
                 list = new ListCollectionView(PLService.GetParcels().ToList());
             }
+            if (state == "From")
+            {
+                list = new ListCollectionView(PLService.GetCustomer((int)id).FromCustomer.ToList());
+            }
+            if (state == "To")
+            {
+                list = new ListCollectionView(PLService.GetCustomer((int)id).ToCustomer.ToList());
+            }
+
         }
         public void OpenDetails(object param)
         {
