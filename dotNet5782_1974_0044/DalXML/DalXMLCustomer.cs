619387cee11a86109c93c40d5125f0c10918ab7c
brachafrid <brachi6721236@gmail.com>
2022-01-23 11:38:51 +0200

    add DalXML

---
+++ dotNet5782_1974_0044/DalXML/DalXMLCustomer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace DalXML
+{
+   partial class DalXML
+    {
+        public void AddCustomer(int id, string phone, string name, double longitude, double latitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
