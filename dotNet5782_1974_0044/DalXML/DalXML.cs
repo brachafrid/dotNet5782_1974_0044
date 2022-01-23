4455e985bea0933e33bbd6b07e437551cb5fbf82
brachafrid <brachi6721236@gmail.com>
2022-01-23 12:13:19 +0200

    changes

--- dotNet5782_1974_0044/DalXML/DalXML.cs
+++ dotNet5782_1974_0044/DalXML/DalXML.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 using Utilities;
 using DLApi;
 using DO;
+using System.IO;
+using System.Xml.Serialization;
 
-namespace DalXML
+namespace Dal
 {
     partial class DalXML : Singletone<DalXML>, IDal
     {
+        public DalXML()
+        {
+            Creation();
+            Initilaztion();
+        }
+        private void Creation()
+        {
+            if (!File.Exists(@"data\XMLDrone"))
+                try
+                {
+                    FileStream file = new FileStream(@"data\XMLDrone", FileMode.Create);
+                    file.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+        }
+
+        private void Initilaztion()
+        {
+
+        }
         public string GetAdministorPasssword()
         {
             throw new NotImplementedException();
