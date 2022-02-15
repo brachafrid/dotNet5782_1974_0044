14b5dfd24886d4a220ace25edffc0eafab57aef6
TamarKleiman <Ttichnut2@gmail.com>
2022-02-15 19:32:55 +0200

    document BO

--- dotNet5782_1974_0044/BL/BO/CustomerToList.cs
+++ dotNet5782_1974_0044/BL/BO/CustomerToList.cs
@@ -2,13 +2,37 @@
 {
     public class CustomerToList
     {
+        /// <summary>
+        /// Customer to list key
+        /// </summary>
         public int Id { get; init; }
+        /// <summary>
+        /// Customer to list name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Customer to list phone
+        /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// Number parcels of the customer that sent and delivered 
+        /// </summary>
         public int NumParcelSentDelivered { get; set; }
+        /// <summary>
+        /// Number parcels of the customer that sent and and delivered 
+        /// </summary>
         public int NumParcelSentNotDelivered { get; set; }
+        /// <summary>
+        /// Number parcels of the customer that recived 
+        /// </summary>
         public int NumParcelReceived { get; set; }
+        /// <summary>
+        /// Number parcels that in way to customer
+        /// </summary>
         public int NumParcelWayToCustomer { get; set; }
+        /// <summary>
+        /// If the customer is not active
+        /// </summary>
         public bool IsNotActive { get; set; }
         public override string ToString()
         {
--- dotNet5782_1974_0044/BL/BO/ParcelAtCustomer.cs
+++ dotNet5782_1974_0044/BL/BO/ParcelAtCustomer.cs
@@ -1,24 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-    namespace BO
+﻿namespace BO
+{
+    public class ParcelAtCustomer
     {
-        public class ParcelAtCustomer
+        /// <summary>
+        /// Parcel at customer key
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Parcel at customer weight
+        /// </summary>
+        public WeightCategories WeightCategory { get; set; }
+        /// <summary>
+        /// Parcel at customer priority
+        /// </summary>
+        public Priorities Priority { get; set; }
+        /// <summary>
+        /// Parcel at customer state
+        /// </summary>
+        public PackageModes State { get; set; }
+        /// <summary>
+        /// The customer in Parcel
+        public CustomerInParcel Customer { get; set; }
+        public override string ToString()
         {
-           public int Id { get; set; }
-           public WeightCategories WeightCategory { get; set; }
-           public Priorities Priority { get; set; }
-           public PackageModes State { get; set; }
-           public CustomerInParcel Customer { get; set; }
-            public override string ToString()
-            {
-                return this.ToStringProperties();
-            }
+            return this.ToStringProperties();
         }
     }
+}
 
 
